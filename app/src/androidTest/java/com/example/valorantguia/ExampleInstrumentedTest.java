df144a48e5ae287b860966d8e13611a50bbab805
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-05-22 18:31:18 -0300

    upgrande

--- app/src/androidTest/java/com/example/valorantguia/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/valorantguia/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.loginapp;
+package com.example.valorantguia;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.loginapp", appContext.getPackageName());
+        assertEquals("com.example.valorantguia", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/valorantguia/ExampleUnitTest.java
+++ app/src/test/java/com/example/valorantguia/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.loginapp;
+package com.example.valorantguia;
 
 import org.junit.Test;
 
