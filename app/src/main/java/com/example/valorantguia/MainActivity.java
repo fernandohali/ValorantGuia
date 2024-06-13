8d771e3f9e0e43a91724a2327b3af65991ac7672
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-06-13 00:54:06 -0300

    criação do Navigation Bar

--- app/src/main/java/com/example/valorantguia/MainActivity.java
+++ app/src/main/java/com/example/valorantguia/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.valorantguia;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.valorantguia.TelaPrincipal.AgentesFragment;
+import com.example.valorantguia.TelaPrincipal.HomeFragment;
+import com.example.valorantguia.databinding.ActivityTelaPrincipalBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityTelaPrincipalBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private boolean onNavigationItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.home) {
+            replaceFragment(new HomeFragment());
+            return true;
+        } else if (item.getItemId() == R.id.agentes) {
+            replaceFragment(new AgentesFragment());
+            return true;
+        }
+        return false;
     }
 }
