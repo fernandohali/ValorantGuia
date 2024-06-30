8754872c0a0ef1341f8e2c77824fd0428ff4ad17
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-06-30 00:25:55 -0300

    updrade

--- app/src/main/java/com/example/valorantguia/TelaPrincipal/HomeFragment.java
+++ app/src/main/java/com/example/valorantguia/TelaPrincipal/HomeFragment.java
@@ -1,14 +1,16 @@
 package com.example.valorantguia.TelaPrincipal;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.valorantguia.R;
+import com.example.valorantguia.maps.bind.bindMapa;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +19,11 @@ import com.example.valorantguia.R;
  */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    ImageButton btnMapaBind;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +31,6 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -60,7 +52,25 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Encontrar o ImageButton pelo ID no layout inflado
+        btnMapaBind = view.findViewById(R.id.btnMapaBind);
+
+        // Configurar o listener do ImageButton
+
+
+        btnMapaBind.setOnClickListener(v -> {
+            bindMapaClic();
+        });
+
+        return view;
     }
+
+    private void bindMapaClic() {
+        Intent intent = new Intent(requireActivity(), bindMapa.class);
+        startActivity(intent);
+    }
+
+
 }
