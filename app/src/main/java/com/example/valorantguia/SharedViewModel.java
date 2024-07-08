270099c770e29c3ef384b7ef6ba11981c9fc374f
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-07-08 01:56:40 -0300

    upgrade

--- app/src/main/java/com/example/valorantguia/SharedViewModel.java
+++ app/src/main/java/com/example/valorantguia/SharedViewModel.java
@@ -7,39 +7,35 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class SharedViewModel extends ViewModel {
-    private final MutableLiveData<Boolean> _isMapaSunsetClicked = new MutableLiveData<>(false);
-    private final MutableLiveData<Boolean> _isMapaIceboxClicked = new MutableLiveData<>(false);
-    private final MutableLiveData<Boolean> _isMapaBindClicked = new MutableLiveData<>(false);
-    private final MutableLiveData<Boolean> _isMapaAscentClicked = new MutableLiveData<>(false);
-    private final MutableLiveData<Boolean> _isMapaSplitClicked = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isMapaSunsetClicked = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isMapaIceboxClicked = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isMapaBindClicked = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isMapaAscentClicked = new MutableLiveData<>(false);
+    private final MutableLiveData<Boolean> isMapaSplitClicked = new MutableLiveData<>(false);
 
-    private final LiveData<Boolean> isMapaSunsetClicked = _isMapaSunsetClicked;
-    private final LiveData<Boolean> isMapaIceboxClicked = _isMapaIceboxClicked;
-    private final LiveData<Boolean> isMapaBindClicked = _isMapaBindClicked;
-    private final LiveData<Boolean> isMapaAscentClicked = _isMapaAscentClicked;
-    private final LiveData<Boolean> isMapaSplitClicked = _isMapaSplitClicked;
 
     // Métodos para definir o estado do clique para cada mapa
     public void setMapaSunsetClicked(boolean clicked) {
         if (Boolean.FALSE.equals(isMapaSunsetClicked.getValue()) && clicked) {
-            _isMapaSunsetClicked.setValue(true);
+            isMapaSunsetClicked.setValue(true);
             Log.d("SharedViewModel", "Mapa Sunset selecionado");
         } else if (!clicked) {
-            _isMapaSunsetClicked.setValue(false);
+            isMapaSunsetClicked.setValue(false);
         }
     }
 
     public LiveData<Boolean> getMapaSunsetClicked() {
         Log.d("SharedViewModel", "getMapaSunsetClicked: " + isMapaSunsetClicked.getValue());
+
         return isMapaSunsetClicked;
     }
 
     public void setMapaIceboxClicked(boolean clicked) {
         if (Boolean.FALSE.equals(isMapaIceboxClicked.getValue()) && clicked) {
-            _isMapaIceboxClicked.setValue(true);
+            isMapaIceboxClicked.setValue(true);
             Log.d("SharedViewModel", "Mapa Icebox selecionado");
         } else if (!clicked) {
-            _isMapaIceboxClicked.setValue(false);
+            isMapaIceboxClicked.setValue(false);
         }
     }
 
@@ -50,10 +46,10 @@ public class SharedViewModel extends ViewModel {
 
     public void setMapaBindClicked(boolean clicked) {
         if (Boolean.FALSE.equals(isMapaBindClicked.getValue()) && clicked) {
-            _isMapaBindClicked.setValue(true);
+            isMapaBindClicked.setValue(true);
             Log.d("SharedViewModel", "Mapa Bind selecionado");
         } else if (!clicked) {
-            _isMapaBindClicked.setValue(false);
+            isMapaBindClicked.setValue(false);
         }
     }
 
@@ -64,12 +60,11 @@ public class SharedViewModel extends ViewModel {
 
     public void setMapaAscentClicked(boolean clicked) {
         if (Boolean.FALSE.equals(isMapaAscentClicked.getValue()) && clicked) {
-            _isMapaAscentClicked.setValue(true);
+            isMapaAscentClicked.setValue(true);
             Log.d("SharedViewModel", "Mapa Ascent selecionado");
         } else if (!clicked) {
-            _isMapaAscentClicked.setValue(false);
-        }
-    }
+            isMapaAscentClicked.setValue(false);
+        }    }
 
     public LiveData<Boolean> getMapaAscentClicked() {
         Log.d("SharedViewModel", "getMapaAscentClicked: " + isMapaAscentClicked.getValue());
@@ -78,10 +73,10 @@ public class SharedViewModel extends ViewModel {
 
     public void setMapaSplitClicked(boolean clicked) {
         if (Boolean.FALSE.equals(isMapaSplitClicked.getValue()) && clicked) {
-            _isMapaSplitClicked.setValue(true);
+            isMapaSplitClicked.setValue(true);
             Log.d("SharedViewModel", "Mapa Split selecionado");
         } else if (!clicked) {
-            _isMapaSplitClicked.setValue(false);
+            isMapaSplitClicked.setValue(false);
         }
     }
 
